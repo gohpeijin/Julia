b173f419d68ce825a966066fc7b70d10be4d267f
Ming Liang <44579558+DrakonDarquesse@users.noreply.github.com>
2021-08-29 04:39:38 +0800

    Improve coverage (#5)
    * renamed some camel cased functions to underscore
    
    * Created branch for ConInt.yml
    
    * added test cases of sorting algorithms
    
    * added more test cases of sorting algorithms
    
    * improved test cases of sorting algorithms
    
    * fix test cases
    
    * improve coverage
    
    * improve coverage of interpolation search
    
    * fix test case
    
    Co-authored-by: PoolOfDeath20 <gervinfungdaxuen@gmail.com>

--- test/sorts.jl
+++ test/sorts.jl
@@ -1,18 +1,31 @@
 @testset "Sorts" begin
     sorts = [
-        BubbleSort!,
-        BucketSort!,
-        CountingSort!,
-        ExchangeSort!,
-        InsertionSort!,
-        MergeSort!,
-        QuickSort!,
-        SelectionSort!,
+        bubble_sort!
+        bucket_sort!
+        counting_sort!
+        exchange_sort!
+        insertion_sort!
+        merge_sort!
+        quick_sort!
+        selection_sort!
     ]
 
     for f in sorts
         x = [3,5,1,4,2]
+        y = [12,4,3,2,1]
+        empty = []
+        one = [1]
+
         f(x)
         @test x == [1,2,3,4,5]
+
+        f(y)
+        @test y == [1,2,3,4,12]
+        
+        f(empty)
+        @test empty == []
+        
+        f(one)
+        @test one == [1]
     end
 end
